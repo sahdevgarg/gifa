78b39c8a6603f06c6f0a30ad8c6b2a51ef630b9d
sahdev garg <sahdevgarg@sahdevs-MacBook-Pro.local>
2015-09-14 12:26:27 +0530

    settings change

--- gifa/wsgi.py
+++ gifa/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+PROJECT_PATH = '/var/www/site/gifa'
+PROJECT_PATH_MAIN = '/var/www/site/gifa/gifa/settings'
+
+sys.path.insert(0, PROJECT_PATH)
+sys.path.append(PROJECT_PATH_MAIN)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gifa.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gifa.settings.staging")
 
 application = get_wsgi_application()

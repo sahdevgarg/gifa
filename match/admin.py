0c898ef00c830552a029c59df7ea4da01f73941f
sahdev garg <sahdev.garg@hindustantimes.com>
2015-09-18 12:33:16 +0530

    team update logic

--- match/admin.py
+++ match/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from match.models import Match
 from django.conf import settings
 from teams.models import Teams 
+from django.db.models import Sum
 
 
 class MatchAdmin(admin.ModelAdmin):
@@ -11,11 +12,7 @@ class MatchAdmin(admin.ModelAdmin):
         super(MatchAdmin, self).save_model(request, obj, form, change)
         team_a = Teams.objects.get(id=obj.team_a.id)
         team_b = Teams.objects.get(id=obj.team_b.id)
-        team_a.total_goal = team_a.total_goal + obj.team_a_goal
-        team_b.total_goal = team_b.total_goal + obj.team_b_goal
         if obj.result_type == 2 or obj.result_type == 3:
-            team_a.games_played = team_a.games_played + 1
-            team_b.games_played = team_b.games_played + 1
             if obj.team_b_goal > obj.team_a_goal:
         		obj.winning_team = obj.team_b
             elif obj.team_b_goal == obj.team_a_goal:
@@ -26,8 +23,32 @@ class MatchAdmin(admin.ModelAdmin):
         	obj.team_b_goal = 0
         	obj.team_a_goal = 0
         	obj.winning_team = None;
-        team_a.save()
-        team_b.save()
         obj.save();
- 
+        team_a.games_played = Match.objects.filter(team_a=team_a,result_type__in=[2,3]).count() + Match.objects.filter(team_b=team_a,result_type__in=[2,3]).count()
+        team_b.games_played = Match.objects.filter(team_a=team_b,result_type__in=[2,3]).count() + Match.objects.filter(team_b=team_b,result_type__in=[2,3]).count()
+        team_a.win = Match.objects.filter(winning_team=team_a,result_type=2).count()
+        team_b.win = Match.objects.filter(winning_team=team_b,result_type=2).count()
+        team_a.draw = Match.objects.filter(team_a=team_a,result_type=3).count() + Match.objects.filter(team_b=team_a,result_type=3).count()
+        team_b.draw = Match.objects.filter(team_a=team_b,result_type=3).count() + Match.objects.filter(team_b=team_b,result_type=3).count()
+        team_a.loss = team_a.games_played - team_a.win - team_a.draw
+        team_b.loss = team_b.games_played - team_b.win - team_b.draw
+
+        team_a_goal1 = (Match.objects.filter(team_a=team_a,result_type__in=[2,3]).aggregate(Sum('team_a_goal')))["team_a_goal__sum"]
+        team_a_goal2 = (Match.objects.filter(team_b=team_a,result_type__in=[2,3]).aggregate(Sum('team_b_goal')))["team_b_goal__sum"]
+        if not team_a_goal1:
+            team_a_goal1 = 0;
+        if not team_a_goal2:
+            team_a_goal2 = 0;
+        team_a.total_goal = team_a_goal1 + team_a_goal2
+        team_b_goal1 = (Match.objects.filter(team_a=team_b,result_type__in=[2,3]).aggregate(Sum('team_a_goal')))["team_a_goal__sum"]
+        team_b_goal2 = (Match.objects.filter(team_b=team_b,result_type__in=[2,3]).aggregate(Sum('team_b_goal')))["team_b_goal__sum"]
+        if not team_b_goal1:
+            team_b_goal1 = 0;
+        if not team_b_goal2:
+            team_b_goal2 = 0;
+        team_b.total_goal = team_b_goal1 + team_b_goal2
+
+        team_a.save();
+        team_b.save();
+
 admin.site.register(Match,MatchAdmin)
--- teams/admin.py
+++ teams/admin.py
@@ -4,5 +4,6 @@ from django.conf import settings
 
 
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ("team_name","team_manager")
+    list_display = ("team_name","team_manager","games_played","win","team_group","draw","loss")
+    search_fields = ['team_name']
 admin.site.register(Teams,TeamAdmin)
